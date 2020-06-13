8c406f31360f377a6d7a867aa4a473efcb523a1c
SimonG <saguti15@g.holycross.edu>
2020-06-13 19:59:45 -0400

    add: created graph with dummy data

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -1,16 +1,54 @@
 import React, { Component } from 'react';
 // import { connect } from 'react-redux';
-// import think creator here
+// import think creator here;
+import { VictoryChart, VictoryLine, VictoryTheme, VictoryLegend } from 'victory';
 
 
 class Graph extends Component {
   render () {
     return (
-      <div className="App">
-          <p>
-            Graph will show here
-          </p>
-      </div>
+        <VictoryChart
+        theme={VictoryTheme.material}
+        domain={{x: [1, 5], y: [0, 14]}}
+      >
+        <VictoryLegend x={125} y={50}
+            orientation='horizontal'
+            gutter={20}
+            style={{boarder: {stroke: 'black'}, title: {fontSize: 20}}}
+            data={[
+                {name: 'One', symbol: {fill: "tomato", type: "star"}},
+                {name: 'Two', symbol: {fill: "orange"}},
+                {name: 'Three', symbol: {fill: "gold"}}
+            ]}
+        />
+        
+        <VictoryLine
+          style={{
+            data: { stroke: "#000" },
+            parent: { border: "1px solid #ccc"}
+          }}
+          data={[
+            { x: 1, y: 2 },
+            { x: 2, y: 3 },
+            { x: 3, y: 5 },
+            { x: 4, y: 4 },
+            { x: 5, y: 7 }
+          ]}
+        />
+        <VictoryLine
+          style={{
+            data: { stroke: "#C43A31" },
+            parent: { border: "1px solid #ccc"}
+          }}
+          data={[
+            { x: 1, y: 5 },
+            { x: 2, y: 6 },
+            { x: 3, y: 8 },
+            { x: 4, y: 9 },
+            { x: 5, y: 5 }
+          ]}
+        />
+      </VictoryChart>
     );
   }
 }
