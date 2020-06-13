82f9808a261c7380fdf1510063dec210a0ef571b
SimonG <saguti15@g.holycross.edu>
2020-06-13 15:35:07 -0400

    Feat: added cards and graph components

---
+++ src/components/Cards.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Cards() {
+    return (
+      <div className="App">
+          <p>
+            Cards will show here
+          </p> 
+      </div>
+    );
+  }
+
+export default Cards
