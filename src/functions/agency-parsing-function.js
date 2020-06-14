13582e1fbf885a4b40330c03d9f70490a2d0bf07
Jason Cho <jason.cho.86@gmail.com>
2020-06-14 14:34:01 -0400

    refactor: updated parsing function for agency exp to reflect last 10 years from now

--- src/functions/agency-parsing-function.js
+++ src/functions/agency-parsing-function.js
@@ -3,6 +3,8 @@ function parseAgencyApi(apiData) {
   let returnArray = [];
   let data = [];
   let dept = {};
+  const currDate = new Date(Date.now());
+  const currYear = currDate.getFullYear();
   // loop through the data retrieved from the API
   for (let i = 0; i < apiData.length; i++) {
     let elem = apiData[i];
@@ -13,13 +15,28 @@ function parseAgencyApi(apiData) {
     }
     // if the name of the expendature is 'total dept' we want to loop through that object and add each years expense for the last five years
     if (elem.agency_expenditures === 'TOTAL DEPT.') {
-      for (let year = 2018; year > 2013; year--) {
+      for (let year = currYear - 10; year <= currYear; year++) {
         let temp = {};
-        if (parseFloat(elem[`fy${year}`])) {
-          temp.x = year;
-          temp.y = parseFloat(elem[`fy${year}`].replace(/,/g, ''));
-
-          data.push(temp);
+        // In the dataset, the years are listed in one of the following
+        // formats:
+        // 1. fyYEAR (e.g. fy2018
+        // 2. fy_YEAR (e.g. fy_2013)
+        const expenseString =
+          elem[`fy${year}`] !== undefined
+            ? elem[`fy${year}`]
+            : elem[`fy_${year}`];
+        // Since dataset is not always complete, there is a chance that
+        // the expense for a given year is undefined
+        if (expenseString !== undefined) {
+          const expenseNumber = parseFloat(expenseString.replace(/,/g, ''));
+          // The expense for an agency may not be in a number format
+          // (e.g. - 0) so we need to make sure the number parses to an
+          // actual number
+          if (!isNaN(expenseNumber)) {
+            temp.x = year.toString();
+            temp.y = parseFloat(expenseString.replace(/,/g, ''));
+            data.push(temp);
+          }
         }
       }
       // finally add our department to the final data set and reset the dept obj so we can be ready for the next one.
