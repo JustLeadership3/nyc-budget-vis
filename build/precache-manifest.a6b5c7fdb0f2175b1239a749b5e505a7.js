33e708d9bff03305dff95b7768b19ac5b002848c
C Gutierrez <gutierrez.clara.es@gmail.com>
2020-06-21 11:55:34 -0700

    adds build folder

---
+++ build/precache-manifest.a6b5c7fdb0f2175b1239a749b5e505a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06b09045e719c4b3a3219320b9a0c0f9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cd5b431845e3739f1e84",
+    "url": "./static/css/main.68cb74a9.chunk.css"
+  },
+  {
+    "revision": "e2771390d7efc5417487",
+    "url": "./static/js/2.a5efa9b4.chunk.js"
+  },
+  {
+    "revision": "89ea5e0ed13930f7b9c9c2f392e27cfe",
+    "url": "./static/js/2.a5efa9b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd5b431845e3739f1e84",
+    "url": "./static/js/main.1c9711f3.chunk.js"
+  },
+  {
+    "revision": "8e8c2ae6f8fbb9ef627f",
+    "url": "./static/js/runtime-main.6d7bba69.js"
+  },
+  {
+    "revision": "2445cf85d7b363d2d78d44d855a7e24d",
+    "url": "./static/media/logo.2445cf85.png"
+  }
+]);
